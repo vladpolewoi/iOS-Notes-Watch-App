ffcb18b34e25bbe46bd3ee3b02116fa492cdfcd7
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-12-11 21:01:04 +0200

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Quest76 on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
